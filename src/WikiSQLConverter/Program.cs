da54ff9c4cc5f066e8138d4000b6963122008f46
Liam Cavanagh <liamca@microsoft.com>
2016-08-04 09:19:27 -0700

    Applications to import Wiki data to SQL Server as well as query Synonyms database

---
+++ src/WikiSQLConverter/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiSQLConverter
+{
+    class Program
+    {
+        private static string SQLConnectionString;
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: WikiSQLConverter.exe [File path of wiki SQL file] [SQL Server Connection String]");
+                return;
+            }
+
+            SQLConnectionString = args[1];
+            ExtractDataToTXT(args[0]);
+
+        }
+
+        static void ExtractDataToTXT(string FileName)
+        {
+            string line;
+            int counter = 0;
+            bool FoundData = false;
+            string InsertString;
+            string InsertIntoString = string.Empty;
+            string ParsedLine = string.Empty;
+            string DataLine = string.Empty;
+            // Read the file and display it line by line.
+            StreamReader file = new System.IO.StreamReader(FileName);
+            using (SqlConnection conn = new SqlConnection(SQLConnectionString))
+            {
+                    conn.Open();
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        if (line.IndexOf("VALUES (") > -1)
+                        {
+                            FoundData = true;
+                            InsertIntoString = line.Substring(0, line.IndexOf("VALUES (") + 7).Replace("`", "");
+                            line = line.Substring(line.IndexOf("VALUES (") + 7);
+                        }
+
+                        if ((FoundData) && (line.IndexOf(",") > -1))
+                        {
+                            InsertString = line.Replace("`", "");
+                            InsertString = InsertString.Replace("\\'", "''");
+                            while (InsertString.Length > 0)
+                            {
+                                // Break into a smaller batch of 500 items
+                                int currIndexLoc = 0;
+                                for (int rowCounter = 0; rowCounter < 500; rowCounter++)
+                                {
+                                    counter++;
+                                    currIndexLoc = InsertString.IndexOf("),(", currIndexLoc + 1);
+                                }
+
+                                if (counter == 1963500)
+                                {
+                                    Console.Write("aa");
+                                }
+
+                                string query = InsertIntoString;
+                                if (currIndexLoc > -1)
+                                {
+                                    query += InsertString.Substring(0, currIndexLoc + 1);
+                                    InsertString = InsertString.Substring(currIndexLoc + 2);
+                                }
+                                else
+                                {
+                                    query += InsertString;
+                                    InsertString = string.Empty;
+                                }
+                                try
+                                {
+                                    SqlCommand cmd = new SqlCommand(query, conn);
+                                    cmd.ExecuteNonQuery();
+                                    Console.WriteLine(counter);
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                }
+                            }
+
+                        }
+
+                    }
+            }
+            file.Close();
+
+        }
+    }
+}
---
+++ src/WikiSynonym/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data;
+
+namespace WikiSynonym
+{
+    class Program
+    {
+        public static SQLiteConnection sqlite;
+    
+        static void Main(string[] args)
+        {
+            if (args.Count() == 0)
+            {
+                Console.WriteLine("Usage: WikiSynonym [word]");
+                return;
+            }
+
+            string word = args[0].ToLower();
+            sqlite = new SQLiteConnection("Data Source=synonyms.db");
+            string query = "with SynonymList as " +
+                "( select synonym from synonyms where root = '" + word + "' " +
+                "union " +
+                "select synonym from synonyms where root in ( " +
+                "  select root from synonyms where synonym = '" + word + "' " +
+                ")) " +
+                "select distinct synonym from SynonymList order by synonym "; 
+            try
+            {
+                SQLiteCommand cmd;
+                sqlite.Open();  //Initiate connection to the db
+
+                using (SQLiteCommand fmd = sqlite.CreateCommand())
+                {
+                    fmd.CommandText = query;
+                    fmd.CommandType = CommandType.Text;
+                    SQLiteDataReader r = fmd.ExecuteReader();
+                    while (r.Read())
+                    {
+                        Console.WriteLine(r["synonym"]);
+                    }
+
+                    //    cmd = sqlite.CreateCommand();
+
+                    //SQLiteDataReader r = fmd.ExecuteReader();
+                    //while (r.Read())
+                    //{
+                    //    ImportedFiles.Add(Convert.ToString(r["FileName"]));
+
+                    //}
+
+                    //cmd.CommandText = query;  //set the passed query
+                    //ad = new SQLiteDataAdapter(cmd);
+                    //ad.Fill(dt); //fill the datasource
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                //Add your exception code here.
+            }
+            sqlite.Close();
+            return;
+        }
+    }
+}
